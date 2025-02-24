b24788d3486d335a9f119598b15387481b066133
Luan Argolo Lemos <ipsluan@icloud.com>
2025-02-24 17:29:22 -0300

    feat(iframe section): add prop display

--- sections/IframeSection.tsx
+++ sections/IframeSection.tsx
@@ -1,1 +1,10 @@
 export { default } from "site/islands/IframeSection.tsx";
+import type { Props } from "site/islands/IframeSection.tsx";
+import { AppContext } from "site/apps/site.ts";
+
+export const loader = async (props: Props, _req: Request, ctx: AppContext) => {
+  return {
+    ...props,
+    device: ctx.device,
+  }
+}
