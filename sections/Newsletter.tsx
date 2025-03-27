b89979e79c26cc9ea057d74c3492251301690912
Luan Lemos <ipsluan@icloud.com>
2025-03-26 21:17:36 -0300

    feat: newsletter

--- sections/Newsletter.tsx
+++ sections/Newsletter.tsx
@@ -1,5 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import { useScript } from "@deco/deco/hooks";
+import BannerUI from "site/components/ui/Banner.tsx";
 
 export default function Newsletter() {
   const loadScript = () => {
@@ -7,6 +8,22 @@ export default function Newsletter() {
     script.src = "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js";
     script.async = true;
     document.body.appendChild(script);
+  };
+
+  const image = {
+    image: {
+      mobile:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4600/2aed496f-8a37-4414-868b-eea522083eed",
+      desktop:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4600/e06d024c-6033-4553-aeb6-3998b49ac8d5",
+      heightMobile: 456,
+    },
+    backgroundImage: {
+      mobile:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4600/59282c4f-d719-482d-b3b9-46bebfe53963",
+      desktop:
+        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4600/ff135d8f-5913-4ba8-a5d3-43e73563685e",
+    },
   };
 
   return (
@@ -25,76 +42,89 @@ export default function Newsletter() {
       >
       </script>
 
-      <div id="mc_embed_shell" class="flex items-center justify-center py-4">
-        <style>
-          {`#mc_embed_signup {
+      <section>
+        <div class="relative flex justify-center bg-[#1E1E1E] min-h-[500px] max-h-[500px] h-[500px]">
+          <BannerUI banner={image} />
+
+          <div class="absolute w-full flex flex-col items-center md:top-9 lg:top-14 px-4 py-2">
+            <h2 class="font-galano text-center font-extrabold text-[32px] md:text-4xl lg:text-5xl text-[#D9D9D9] sm:mb-6 lg:mb-12 sm:mt-6">
+              EVCF News – Acompanhe as Novidades do Ecossistema
+            </h2>
+
+            <div class="w-full">
+              <div
+                id="mc_embed_shell"
+                class="flex items-center justify-center py-4"
+              >
+                <style>
+                  {`#mc_embed_signup {
           background: #fff;
           clear: left;
           font: 14px Helvetica, Arial, sans-serif;
           width: 600px;
         }`}
-        </style>
-        <div id="mc_embed_signup">
-          <form
-            action="https://emergingvcfellows.us8.list-manage.com/subscribe/post?u=8c991a6b91e52ff3336aa1f8c&amp;id=9279b6471c&amp;f_id=00bac1e2f0"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            class="validate"
-            target="_blank"
-          >
-            <div id="mc_embed_signup_scroll">
-              <h2>Newsletter</h2>
-              <div class="indicates-required">
-                <span class="asterisk">*</span> indica obrigatório
-              </div>
-              <div class="mc-field-group">
-                <label htmlFor="mce-EMAIL">
-                  Endereço de e-mail <span class="asterisk">*</span>
-                </label>
-                <input
-                  type="email"
-                  name="EMAIL"
-                  class="required email"
-                  id="mce-EMAIL"
-                  required
-                />
-              </div>
-              <div id="mce-responses" class="clear foot">
-                <div
-                  class="response"
-                  id="mce-error-response"
-                  style={{ display: "none" }}
-                >
-                </div>
-                <div
-                  class="response"
-                  id="mce-success-response"
-                  style={{ display: "none" }}
-                >
-                </div>
-              </div>
-              <div
-                aria-hidden="true"
-                style={{ position: "absolute", left: "-5000px" }}
-              >
-                <input
-                  type="text"
-                  name="b_8c991a6b91e52ff3336aa1f8c_9279b6471c"
-                  tabIndex={-1}
-                  defaultValue=""
-                />
-              </div>
-              <div class="optionalParent">
-                <div class="clear foot">
-                  <input
-                    type="submit"
-                    name="subscribe"
-                    id="mc-embedded-subscribe"
-                    class="button"
-                    value="Inscreva-se"
-                  />
-                  {/* <p style={{ margin: "0px auto" }}>
+                </style>
+                <div id="mc_embed_signup">
+                  <form
+                    action="https://emergingvcfellows.us8.list-manage.com/subscribe/post?u=8c991a6b91e52ff3336aa1f8c&amp;id=9279b6471c&amp;f_id=00bac1e2f0"
+                    method="post"
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    class="validate"
+                    target="_blank"
+                  >
+                    <div id="mc_embed_signup_scroll">
+                      <div class="indicates-required">
+                        <span class="asterisk">*</span> indica obrigatório
+                      </div>
+                      <div class="mc-field-group">
+                        <label htmlFor="mce-EMAIL">
+                          Endereço de e-mail <span class="asterisk">*</span>
+                        </label>
+                        <input
+                          type="email"
+                          name="EMAIL"
+                          class="required email"
+                          id="mce-EMAIL"
+                          required
+                        />
+                      </div>
+                      <div id="mce-responses" class="clear foot">
+                        <div
+                          class="response"
+                          id="mce-error-response"
+                          style={{ display: "none" }}
+                        >
+                        </div>
+                        <div
+                          class="response"
+                          id="mce-success-response"
+                          style={{ display: "none" }}
+                        >
+                        </div>
+                      </div>
+                      <div
+                        aria-hidden="true"
+                        style={{ position: "absolute", left: "-5000px" }}
+                      >
+                        <input
+                          type="text"
+                          name="b_8c991a6b91e52ff3336aa1f8c_9279b6471c"
+                          tabIndex={-1}
+                          defaultValue=""
+                        />
+                      </div>
+                      <div class="optionalParent">
+                        <div class="clear foot">
+                          <input
+                            type="submit"
+                            name="subscribe"
+                            id="mc-embedded-subscribe"
+                            class="button"
+                            value="Inscreva-se"
+                          />
+                          {
+                            /* <p style={{ margin: "0px auto" }}>
                     <a
                       href="http://eepurl.com/i19BFs"
                       title="Mailchimp - marketing por e-mail fácil e divertido"
@@ -121,13 +151,18 @@ export default function Newsletter() {
                         />
                       </span>
                     </a>
-                  </p> */}
+                  </p> */
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
-          </form>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
